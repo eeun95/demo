a3312d67f29b0794ec3c5db42c35ae5fdb2d0ea2
eeun <eeun95@gmail.com>
2023-02-05 18:36:13 +0900

    커피 주문 api

--- src/main/java/com/myproject/demo/domain/Orders.java
+++ src/main/java/com/myproject/demo/domain/Orders.java
@@ -3,11 +3,8 @@ package com.myproject.demo.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Entity
-public class Order {
+public class Orders {
     @Id
     private Long id;
     //private Map<String, Integer> receipt = new HashMap<>();
