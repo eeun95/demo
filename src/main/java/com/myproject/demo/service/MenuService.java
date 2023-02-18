fd03e0bdd8817e2fb037b65ddb6d4745c875cef5
eeun <eeun95@gmail.com>
2023-02-18 13:57:06 +0900

    커피 주문 api

--- src/main/java/com/myproject/demo/service/MenuService.java
+++ src/main/java/com/myproject/demo/service/MenuService.java
@@ -8,7 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +29,16 @@ public class MenuService implements MenuServiceInterface {
         List<String> coffeeList = receiptRepository.showHotMenu(LocalDateTime.now().minusDays(7), LocalDateTime.now());
         log.info("hotMenu {}", coffeeList);
         return coffeeList;
+    }
+
+    public Map<String, Integer> getMenuPrice() {
+        List<Coffee> coffeeList = menuRepository.findAll();
+
+        Map<String, Integer> priceList = new HashMap<>();
+        for (Coffee coffee : coffeeList) {
+            priceList.put(String.valueOf(coffee.getName()), coffee.getPrice());
+        }
+        log.info("menu {}{}",priceList, coffeeList);
+        return priceList;
     }
 }
