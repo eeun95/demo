176df6ee2612911a0d0df3744043cbb6b079063f
eeun <eeun95@gmail.com>
2023-02-18 13:52:10 +0900

    커피 주문 api

--- src/test/java/com/myproject/demo/service/PointServiceTest.java
+++ src/test/java/com/myproject/demo/service/PointServiceTest.java
@@ -2,13 +2,17 @@ package com.myproject.demo.service;
 
 import com.myproject.demo.Dto.request.OrderRequestDto;
 import com.myproject.demo.Dto.request.PointRequestDto;
+import com.myproject.demo.Dto.response.OrderResponseDto;
 import com.myproject.demo.Exception.LackOfPointException;
 import com.myproject.demo.Exception.MemberNotFoundException;
 import com.myproject.demo.domain.Menu.Coffee;
 import com.myproject.demo.domain.Orders;
 import com.myproject.demo.domain.Point;
 import com.myproject.demo.domain.PointHistory;
+import com.myproject.demo.domain.Receipt;
 import com.myproject.demo.repository.MenuRepository;
+import com.myproject.demo.repository.OrderRepository;
+import com.myproject.demo.repository.Receipt.ReceiptRepository;
 import com.myproject.demo.repository.point.PointHistoryRepository;
 import com.myproject.demo.repository.point.PointRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,6 +41,12 @@ class PointServiceTest {
 
     @Autowired
     MenuRepository menuRepository;
+
+    @Autowired
+    ReceiptRepository receiptRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
 
     @Test
     void 포인트_충전() {
@@ -85,6 +95,46 @@ class PointServiceTest {
         Map<String, Integer> priceList = new HashMap<>();
         for (Coffee coffee : coffeeList) {
             priceList.put(String.valueOf(coffee.getName()), coffee.getPrice());
+        }
+
+        int sumPrice = 0;
+        for (String coffeeName : menu.keySet()) {
+            int price = priceList.get(coffeeName)*menu.get(coffeeName);
+            sumPrice += price;
+            Receipt receipt = Receipt.builder()
+                    .orders(orders)
+                    .coffeeName(coffeeName)
+                    .count(menu.get(coffeeName))
+                    .price(price)
+                    .build();
+            receiptRepository.save(receipt);
+            orders.setReceipts(receipt);
+        }
+        orders.setTotalPrice(sumPrice);
+        Orders saveOrders = orderRepository.save(orders);
+
+        OrderResponseDto orderResponseDto = new OrderResponseDto(saveOrders);
+
+        Long memberId = orderResponseDto.getMemberId();
+        int totalPrice = orderResponseDto.getTotalPrice();
+        Optional<Point> pointObj = pointRepository.findByMemberId(memberId);
+
+        if(pointObj.isEmpty()) {
+            throw new MemberNotFoundException();
+        } else {
+            Point memberPoint = pointObj.get();
+            if (memberPoint.getTotalPoint() < totalPrice) {
+                throw new LackOfPointException();
+            } else {
+                int remainPoint = memberPoint.getTotalPoint() - totalPrice;
+                memberPoint.updatePoint(remainPoint);
+                pointRepository.save(memberPoint);
+
+                pointHistoryRepository.save(PointHistory.builder()
+                        .point(memberPoint)
+                        .usePoint(totalPrice)
+                        .build());
+            }
         }
     }
 }
