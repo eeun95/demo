bf7a3242cd1be1ab9d176b2f39d03b12d26b5bf4
eeun <eeun95@gmail.com>
2023-02-27 12:17:50 +0900

    멀티 모듈 QUERY DSL 적용

--- application/src/main/java/com/myproject/demo/application/ApiApplication.java
+++ application/src/main/java/com/myproject/demo/application/ApiApplication.java
@@ -2,8 +2,12 @@ package com.myproject.demo.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.myproject.demo.domain")
+@EnableJpaRepositories(basePackages = "com.myproject.demo.domain")
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
---
+++ domain/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into coffee(name, price) values ('Americano', 4500);
+insert into coffee(name, price) values ('Latte', 5500);
+insert into coffee(name, price) values ('Coldbrew', 6500);
+insert into coffee(name, price) values ('Cappuccino', 5500);
+insert into coffee(name, price) values ('Espresso', 2500);
